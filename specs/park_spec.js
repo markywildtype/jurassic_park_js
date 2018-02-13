7a817a67dcd8eb0287e432f05f015b09fe5d46a8
Mark Blanford <markytorture@hotmail.com>
2018-02-13 17:32:49 +0000

    Park class added with a name attribute

---
+++ specs/park_spec.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const Park = require('../park.js')
+const Dinosaur = require('../dinosaur.js')
+
+describe('Park', function(){
+
+  let park;
+  let velociraptor;
+  let tRex;
+
+  beforeEach(function(){
+    park = new Park('Jurassic Park');
+    velociraptor = new Dinosaur('Velociraptor', 6);
+    tRex = new Dinosaur('Tyrannosaurus Rex', 2);
+  });
+
+  it('should have a name', function(){
+    actual = park.name;
+    assert.strictEqual(actual, 'Jurassic Park');
+  })
+
+
+
+
+
+
+});
